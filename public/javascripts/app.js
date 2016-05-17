676b04f2e251344c0c3d26b18f887c16db5c88c1
amhayslip <amhayslip@gmail.com>
2016-05-17 20:03:40 +0300

    init

---
+++ public/javascripts/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('redditFun', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/templates/home.html',
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+       }
+    })
+    .state('post', {
+      url: '/posts/:id',
+      templateUrl: '/templates/posts.html',
+      controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
+    })
+
+  $urlRouterProvider.otherwise('home');
+}]);
---
+++ public/javascripts/services/posts.js
@@ -0,0 +1,39 @@
+app.factory('posts', ['$http', function($http) {
+  var postService = {
+    posts: [],
+
+    getAll: function() {
+      return $http.get('/posts').then(function(data) {
+  
+        angular.copy(data.data, postService.posts);
+      });
+    },
+
+    get: function(id) {
+      return $http.get('/posts/' + id).then(function(res){
+        return res.data;
+      });
+    },
+
+    create: function(post) {
+      return $http.post('/posts', post).success(function(data){
+        postService.posts.push(data);
+      });
+    },
+
+    upvote: function(post) {
+      // TODO: Finish
+    },
+
+    addComment: function(id, comment) {
+      return $http.post('/posts/' + id + '/comments', comment);
+    },
+
+    upvoteComment: function(post, comment) {
+      // TODO: Finish
+    }
+  };
+  
+
+  return postService;
+}]);
